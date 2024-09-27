1f628f3cd6cb1f8ec7a5f02da6a0a90ecb6c0f5b
LasseNissen <144016776+ln13foqy@users.noreply.github.com>
2024-09-27 15:45:37 +0200

    Add files via upload

--- run_gwswuse.py
+++ run_gwswuse.py
@@ -15,7 +15,7 @@ Run GWSWUSE.
 import time
 from termcolor import colored
 from controller import configuration_module as cm
-from controller import input_data_manager_new as idm
+from controller import input_data_manager as idm
 
 from model.irrigation_simulation import IrrigationSimulator
 from model.domestic_simulation import DomesticSimulator
@@ -24,28 +24,29 @@ from model.thermal_power_simulation import ThermalPowerSimulator
 from model.livestock_simulation import LivestockSimulator
 from model.total_sectors_simulation import TotalSectorsSimulator
 
-from view.output_data_manager_new import output_data_manager
+from view.output_data_manager import output_data_manager
 
 
-def run_gwswuse():
+def run_regwswuse():
     """Run the linking module GWSWUSE of WaterGAP."""
     start_time = time.time()
     # Load, check and preprocess input data with input_data_manager
     preprocessed_gwswuse_data, gwswuse_check_results, _, _ = \
-        idm.input_data_manager(cm.input_path,
+        idm.input_data_manager(cm.input_data_path,
                                cm.gwswuse_convention_path,
                                cm.start_year,
                                cm.end_year,
-                               cm.time_extend_mode,
-                               cm.correct_irr_with_t_aai_mode
+                               cm.time_extend_mode
                                )
+    print('GWSWUSE simulation starts ...')
     # Sector-specific simulations
     irr = IrrigationSimulator(preprocessed_gwswuse_data['irrigation'])
     dom = DomesticSimulator(preprocessed_gwswuse_data['domestic'])
     man = ManufacturingSimulator(preprocessed_gwswuse_data['manufacturing'])
     tp = ThermalPowerSimulator(preprocessed_gwswuse_data['thermal_power'])
     liv = LivestockSimulator(preprocessed_gwswuse_data['livestock'])
-    # Summarize sector-specific results for total cross-sector results
+
+    # # Summarize sector-specific results for total cross-sector results
     total = TotalSectorsSimulator(irr, dom, man, tp, liv)
 
     gwswuse_results = {
@@ -59,10 +60,13 @@ def run_gwswuse():
     output_data_manager(gwswuse_results, cm.output_selection, cm.output_dir,
                         cm.start_year, cm.end_year)
     end_time = time.time()
-    print(f"GWSWUSE software runtime: {end_time - start_time} seconds.")
-    return preprocessed_gwswuse_data, gwswuse_check_results, gwswuse_results
+    print(f"ReGWSWUSE software runtime: {end_time - start_time} seconds.")
+    # return preprocessed_gwswuse_data, gwswuse_check_results
+    return gwswuse_check_results, gwswuse_results
 
 
 if __name__ == "__main__":
-    preprocessed_gwswuse_data, gwswuse_check_results, gwswuse_results = \
-        run_gwswuse()
+    gwswuse_check_results, gwswuse_results = \
+        run_regwswuse()
+    # preprocessed_gwswuse_data, gwswuse_check_results = \
+    #     run_regwswuse()
